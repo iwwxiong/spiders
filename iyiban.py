1b76dead9edf83741f549752d64555f23f14304c
wwx <xhq@yiban.cn>
2016-02-03 10:47:47 +0800

    增加登录逻辑

--- iyiban.py
+++ iyiban.py
@@ -4,19 +4,29 @@
 import os
 import re
 import csv
+import json
 import asyncio
 import aiohttp
 import logging
+from functools import partial
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-HIT = re.compile(r'<div\sclass="yb-for-img">.+?<a\shref="(.*?)">.+?<img\ssrc="(.*?)"\swidth.*?>.*?title="(.*?)"\>', re.DOTALL)
+HIT = re.compile(r'<li\sclass="courses-listing-item">.+?<a\shref="(.*?)">.+?<img\ssrc="(.*?)".+?title="(.*?)"', re.DOTALL)
 JED = re.compile(r'<ul\sclass="fcw\sclearfix">.+?<li>提供学校：(.*?)<\/li>.+?<span>(\d+?)<\/span>.+?<li\s.+?>课程代码：(.*?)<\/li>', re.DOTALL)
 Z = re.compile(r'<em\sid="favorites">(\d+?)<\/em>', re.DOTALL)
 C = re.compile(r'<em\sid="unfavorites">(\d+?)<\/em>', re.DOTALL)
 N = re.compile(r'<h1\sclass="fcw".*?">(.*?)<\/h1>', re.DOTALL)
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36'
+HOST = 'http://www.iyiban.cn'
+ORIGIN = HOST
+LOGIN_URL = HOST + '/login'
+LOGIN_AJAX = HOST + '/yiban_account/login_ajax'
+USERNAME = 'username'
+PASSWORD = 'password'
 
 
 class IyibanCrawl(object):
@@ -29,13 +39,50 @@ class IyibanCrawl(object):
     def __init__(self, maxtasks=20):
         self.csvfile = 'c:\\iyiban\\iyiban.csv'
         self.path = 'c:\\iyiban'
-        self.host = 'http://www.iyiban.cn'
+        self.host = HOST
         self.urls = set()
         self.sem = asyncio.Semaphore(maxtasks)
 
     @asyncio.coroutine
+    def get_login(self):
+        """
+        """
+        session = yield from aiohttp.get(LOGIN_URL)
+        self.csrf_token = session.cookies['csrftoken'].value
+        self.cookies = session.cookies
+        yield from session.release()
+
+    @asyncio.coroutine
+    def login(self, username, password):
+        """
+        """
+        yield from self.get_login()
+        s = yield from aiohttp.post(LOGIN_AJAX, data={
+            'email': username,
+            'password': password,
+        }, headers={
+            'X-CSRFToken': self.csrf_token,
+            'User-Agent': USER_AGENT,
+            'Referer': LOGIN_URL,
+            'Origin': ORIGIN,
+            'X-Requested-With': 'XMLHttpRequest',
+        }, cookies=self.cookies)
+        self.get = partial(aiohttp.get, headers={
+            'X-CSRFToken': self.csrf_token
+        }, cookies=s.cookies)
+        a = yield from s.json()
+        if a['success']:
+            print('登录成功：', s.status)
+        else:
+            print('登录失败：', a['value'])
+        yield from s.release()
+
+    @asyncio.coroutine
     def run(self, page=1):
-        r = yield from aiohttp.get(''.join([self.host, '/courses/page/', str(page)]))
+        if not hasattr(self, 'get'):
+            yield from self.login(USERNAME, PASSWORD)
+
+        r = yield from self.get(''.join([self.host, '/courses/page/', str(page)]))
         content = yield from r.text()
         yield from r.release()
         yield from self.sem.acquire()
@@ -67,22 +114,24 @@ class IyibanCrawl(object):
         """
         读取课程信息
         """
-        r = yield from aiohttp.get(u)
+        r = yield from self.get(u)
         content = yield from r.text()
         yield from r.release()
 
-        j, e, d = JED.findall(content)[0]
-        n, z, c = (
-            N.findall(content)[0],
-            Z.findall(content)[0][0],
-            C.findall(content)[0][0],
-        )
+        cont = JED.findall(content)
+        if cont:
+            j, e, d = JED.findall(content)[0]
+            n, z, c = (
+                N.findall(content)[0],
+                Z.findall(content)[0][0],
+                C.findall(content)[0][0],
+            )
 
-        print('Course name: %s' % n)
-        yield from self.sem.acquire()
-        print(self.sem)
-        task = asyncio.Task(self.write_csv(n, j, d, e, z, c))
-        task.add_done_callback(lambda x: self.sem.release())
+            print('Course name: %s' % n)
+            yield from self.sem.acquire()
+            print(self.sem)
+            task = asyncio.Task(self.write_csv(n, j, d, e, z, c))
+            task.add_done_callback(lambda x: self.sem.release())
 
     @asyncio.coroutine
     def write_csv(self, *args):
@@ -103,7 +152,7 @@ class IyibanCrawl(object):
     #@asyncio.coroutine
     async def download_image(self, u, n):
         f = os.path.join(self.path, n)
-        r = await aiohttp.get(u)
+        r = await self.get(u)
 
         with open(f, 'wb') as fd:
             # fd.write(yield from r.read()) 会报语法错误，只能使用await。WTF
@@ -114,4 +163,5 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     i = IyibanCrawl()
     asyncio.Task(i.run())
+
     loop.run_forever()
